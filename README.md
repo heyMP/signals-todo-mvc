962dda2394b3ffe6b8523a5527c9b44d12500a15
Michael Potter <mpotter@redhat.com>
2024-07-03 12:46:56 -0400

    init commit 🔥

---
+++ README.md
@@ -0,0 +1,4 @@
+# Signals Lit TodoMVC
+
+This is an example implementation of the [TodoMVC](https://todomvc.com/) app
+using [@heymp/signals](https://www.npmjs.com/package/@heymp/signals) for state management and [Lit](https://www.npmjs.com/package/lit) for rendering.
---
+++ src/store.ts
@@ -0,0 +1,90 @@
+import * as Signal from '@heymp/signals';
+
+export interface Todo {
+    id: string;
+    text: string;
+    completed: boolean;
+}
+
+export type TodoEdit = Partial<Todo> & { id: string };
+
+const todoFilters = ["all", "active", "completed"] as const;
+
+export type TodoFilter = (typeof todoFilters)[number];
+
+function isTodoFilter(value: string | undefined): value is TodoFilter {
+    return todoFilters.includes(value as TodoFilter);
+}
+
+export class TodosSignal extends Signal.State<Todo[]> {
+  constructor(value: Todo[]) {
+    super(value);
+  }
+
+  add(text: string) {
+    const todo = {
+      text,
+      completed: false,
+      id: window.crypto.randomUUID(),
+    } satisfies Todo;
+		this.value = [...this.value, todo];
+  }
+
+  delete(id: string) {
+    this.value = this.value.filter(i => i.id !== id);
+  }
+
+	removeTodo(id: string) {
+		this.value = this.value.filter(i => i.id !== id);
+	}
+
+	update(edit: TodoEdit) {
+    this.value = this.value.map(i => {
+      if (i.id === edit.id) {
+        return Object.assign(i, edit);
+      }
+      return i;
+    });
+	}
+
+  toggleAll() {
+    // First pass to see if all the TODOs are completed. If all the
+    // todos are completed, we'll set them all to active
+    const allComplete = this.value.every((todo) => todo.completed);
+
+    // Replace the list to trigger updates
+    this.value = this.value.map((todo) => ({
+        ...todo,
+        completed: !allComplete,
+    }));
+  }
+
+  clearCompleted() {
+    this.value = this.value.filter(i => !i.completed);
+  }
+}
+
+export const todos = new TodosSignal([]);
+export const filter = new Signal.State<TodoFilter>(getFilter());
+export const filteredTodos = new Signal.Computed(() => todos.value.filter(i => {
+    switch (filter.value) {
+        case "active":
+            return !i.completed;
+        case "completed":
+            return i.completed;
+    }
+    return i;
+}), [todos, filter]);
+export const completedTodos = new Signal.Computed(() => todos.value.filter(i => i.completed), [todos]);
+
+window.addEventListener("hashchange", () => {
+  filter.value = getFilter();
+});
+
+function getFilter(): TodoFilter {
+  let filter = /#\/(.*)/.exec(window.location.hash)?.[1];
+  if (isTodoFilter(filter)) {
+    return filter;
+  }
+  return 'all';
+}
